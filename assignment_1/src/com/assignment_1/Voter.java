f6966a7a80abe2dff33044d0c9be95b5dcc74e4a
sangram-king <sangramvazratkar@gmail.com>
2025-07-03 17:15:27 +0530

    Assignment 1

---
+++ assignment_1/src/com/assignment_1/Voter.java
@@ -0,0 +1,37 @@
+package com.assignment_1;
+import java.util.Scanner;
+
+class User{
+	int age;
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public void input()
+	{
+	System.out.println("Enter Your Age ");
+	age = sc.nextInt();
+	
+	
+	if(age>18 && age<65) {
+		System.out.println("You are eligible for blood donation");
+	}
+	
+	else {
+		System.out.println("Not eligible for blood donation");
+		input();
+	}
+}
+}
+public class Voter {
+
+	public static void main(String[] args) {
+		
+		User ur = new User();
+		ur.input();
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
